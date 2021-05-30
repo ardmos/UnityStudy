660d9a56afe867e05b3d35dd73f5ccc3fa5931c6
ardmos <46891528+ardmos@users.noreply.github.com>
2021-05-30 21:35:37 +0900

    .

---
+++ Sample_TCPClient/Assets/Scripts/TCPClientController.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using UnityEngine;
+using UnityEngine.UI;
+using System.IO;
+
+public class TCPClientController : MonoBehaviour
+{
+    public Text text;
+    public InputField inputField;
+
+
+    TcpClient client;
+    NetworkStream ns;
+    //StreamWriter 
+
+
+    public void StartConnect()
+    {
+        string ip = "127.0.0.1";
+        int port = 11000;
+
+        try
+        {
+            PrintMsg("서버에 접속을 시도중입니다...");
+            client = new TcpClient(ip, port);
+            ns = client.GetStream();
+            PrintMsg("접속을 성공했습니다. 해피채팅 ^ㅡ^");
+        }
+        catch (System.Exception ex)
+        {
+            Debug.Log(ex.Message + " __From StartConnect()");
+        }
+    }
+
+    public void StopConnect()
+    {
+        ns.Close();
+        client.Close();        
+    }
+
+
+    public void PrintMsg(string msg)
+    {
+        text.text += "\n >> "+msg;
+    }
+
+    public void SendMsgToServer(string msg)
+    {
+        //메세지 문자열을 바이트배열로 바꿔서 넣어주고
+        char[] msgCharArr = msg.ToCharArray();        
+        byte[] msgBytes = Encoding.Default.GetBytes(msgCharArr, 0, msgCharArr.Length);
+                
+
+        ns.Write(msgBytes, 0, msgBytes.Length);
+        ns.Flush();
+
+    }
+
+
+
+
+    #region 버튼 관리
+    public void OnButtonSendClicked()
+    {
+        string msg = inputField.text;
+        PrintMsg(msg);
+        SendMsgToServer(msg);                
+    }
+    public void OnButtonAccessClicked()
+    {
+        StartConnect();
+    }
+    public void OnButtonDisconnectClicked()
+    {
+        StopConnect();
+    }        
+    #endregion
+}
---
+++ Sample_TCPServer/Assets/Scipts/TCPServerController.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TCPServerController : MonoBehaviour
+{
+    public Text text;
+    public InputField inputField;
+
+    TcpListener listener;
+    TcpClient client;
+    NetworkStream ns;
+
+    bool done = false;
+    bool startServer = false;
+
+    void StartServer()
+    {
+        try
+        {
+            PrintMsg("서버가 시작됩니다...");
+            listener = new TcpListener(IPAddress.Any, 11000);
+            listener.Start();
+            PrintMsg("서버가 시작되었습니다.");
+            //StartListening();
+            //StartCoroutine(StartListening());
+            //startServer = true;
+            PrintMsg("접속을 기다립니다...");
+            listener.BeginAcceptTcpClient(new System.AsyncCallback(StartListening), listener);
+            //print("아직..");
+        }
+        catch (System.Exception ex)
+        {
+            Debug.Log(ex.Message);
+        }
+    }
+
+    private void Update()
+    {
+        if (client != null)
+        {
+            if (client.Connected)
+            {
+                PrintMsg("클라이언트가 연결되었습니다.");
+            }
+            else
+            {
+                ns.Close();
+                client.Close();
+                PrintMsg("클라이언트 연결이 끊어졌습니다.");
+            }
+        }
+        else print("client is null");
+        
+    }
+
+    void StartListening(System.IAsyncResult asyncResult)
+    {
+        PrintMsg("요스!");
+        try
+        {
+            TcpListener listener = (TcpListener)asyncResult.AsyncState;
+
+
+            client = listener.EndAcceptTcpClient(asyncResult);
+
+            
+            ns = client.GetStream();
+            PrintMsg("접속됐습니다!");
+
+        }
+        catch (System.Exception ex)
+        {
+            PrintMsg(ex.Message);
+        }
+    }
+
+    public void PrintMsg(string msg)
+    {
+        text.text += "\n >> " + msg;
+    }
+
+    #region 본격적 대화
+    void DoChat()
+    {
+        byte[] msgbytes = new byte[1024];
+        int bytesRead = ns.Read(msgbytes, 0, msgbytes.Length);
+        string clientmsg = Encoding.Default.GetString(msgbytes, 0, bytesRead);
+        PrintMsg(clientmsg);
+    }
+    #endregion
+
+
+    #region 버튼 관리
+    public void OnButtonSendClicked()
+    {
+
+    }
+    public void OnButtonStartServerClicked()
+    {
+        StartServer();
+    }
+    public void OnButtonStopServerClicked()
+    {
+
+    }
+    #endregion
+    
+}
